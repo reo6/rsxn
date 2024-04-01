3b834c0517512e4d4e0597ad0f04ab5a58fa329e
reo <ramazanemreosmanoglu@gmail.com>
2024-04-02 00:18:18 +0300

    Add main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+pub mod launcher;
+pub mod gui;
+
+use launcher::ServerLauncher;
+use gui::gui::RsxnGUI;
+use std::sync::mpsc;
+use std::sync::{Arc, Mutex};
+
+
 fn main() {
-    println!("Hello, world!");
+    env_logger::init();
+
+    let (sender, receiver) = mpsc::channel();
+
+    let server_launcher = Arc::new(Mutex::new(ServerLauncher::new(
+        std::env::var("PAPER_JAR_PATH").expect("PAPER_JAR_PATH not set"),
+        std::env::var("JAVA_EXE_PATH").expect("JAVA_EXE_PATH not set"),
+        std::env::var("RSXN_SERVER_PATH").expect("RSXN_SERVER_PATH not set"),
+        vec![],
+        "Test Server".to_string(),
+        2048,
+        Arc::new(sender),
+    )));
+
+    let server_launcher_clone = Arc::clone(&server_launcher);
+
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "rsxn",
+        native_options,
+        Box::new(move |cc| Box::new(RsxnGUI::new_with_receiver(receiver, server_launcher_clone.clone())))
+    );
 }
