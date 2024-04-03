1504da06f84dd3ca96c441d745270734e941ed3d
reo <ramazanemreosmanoglu@gmail.com>
2024-04-03 17:12:55 +0300

    gui: Use return key to send command

--- src/gui/gui.rs
+++ src/gui/gui.rs
@@ -1,6 +1,6 @@
+use crate::launcher::ServerLauncher;
 use eframe::egui;
 use std::sync::mpsc::Receiver;
-use crate::launcher::ServerLauncher;
 use std::sync::{Arc, Mutex};
 
 pub struct RsxnGUI {
@@ -11,7 +11,10 @@ pub struct RsxnGUI {
 }
 
 impl RsxnGUI {
-    pub fn new_with_receiver(log_stream_receiver: Receiver<String>, launcher: Arc<Mutex<ServerLauncher>>) -> RsxnGUI {
+    pub fn new_with_receiver(
+        log_stream_receiver: Receiver<String>,
+        launcher: Arc<Mutex<ServerLauncher>>,
+    ) -> RsxnGUI {
         RsxnGUI {
             command_input: String::new(),
             log_stream_receiver,
@@ -38,38 +41,44 @@ impl eframe::App for RsxnGUI {
                     .max_height(300.0)
                     .stick_to_bottom(true)
                     .show(ui, |ui| {
-                        ui.add(egui::TextEdit::multiline(&mut logs_for_textedit.clone())
-                            .font(egui::FontId::new(13.0, egui::FontFamily::Monospace))
-                            .desired_width(f32::INFINITY)
-                            .desired_rows(10)
+                        ui.add(
+                            egui::TextEdit::multiline(&mut logs_for_textedit.clone())
+                                .font(egui::FontId::new(13.0, egui::FontFamily::Monospace))
+                                .desired_width(f32::INFINITY)
+                                .desired_rows(10),
                         );
-                    }
-                );
+                    });
 
                 let mut launcher = self.launcher.lock().unwrap();
 
                 ui.horizontal(|ui| {
-                    ui.add(egui::TextEdit::singleline(&mut self.command_input)
-                        .hint_text("Enter a Command...")
-                    );
-                    if ui.button("Send").clicked() {
+                    let command_input = egui::TextEdit::singleline(&mut self.command_input)
+                        .hint_text("Enter a Command...");
+
+                    let command_input_response = ui.add(command_input);
+
+                    if ui.input(|i| i.key_pressed(egui::Key::Enter))
+                        && command_input_response.lost_focus()
+                        && !self.command_input.is_empty()
+                        && (launcher.state == crate::launcher::ServerState::RUNNING)
+                    {
                         launcher.send_command(self.command_input.clone());
                         self.command_input.clear();
+                        command_input_response.request_focus();
                     }
                 });
 
-
                 ui.horizontal(|ui| {
-
                     if ui.button("Start").clicked() {
                         self.logs.clear();
                         launcher.launch();
-                    } else if (launcher.state == crate::launcher::ServerState::RUNNING) && ui.button("Stop").clicked() {
+                    } else if (launcher.state == crate::launcher::ServerState::RUNNING)
+                        && ui.button("Stop").clicked()
+                    {
                         launcher.stop();
                     }
                 })
             })
-
         });
     }
 }
